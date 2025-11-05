4682a7ad9fdc8bb133ef14806ee6e9fb734bae44
z01aux <zblip01@gmail.com>
2025-11-05 16:41:19 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false,
-  },
-  server: {
-    port: 3000,
-  },
-  // Especificar explícitamente la ubicación del index.html
-  root: '.',
-  publicDir: 'public',
+  }
 })
